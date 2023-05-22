655361c112b3731f148012c586a1693dec477c02
Kabeer EA <kabeerp42@gmail.com>
2023-05-22 01:35:50 +0100

    created utility function for error and success response

--- server/api/src/helpers/utility.ts
+++ server/api/src/helpers/utility.ts
@@ -1,13 +1,21 @@
-import { isString } from "./validation"
+import { isString } from "./validation";
 
-export function createResponse (data: any) {
-    if(isString(data)){
-        return {
-            status: "",
-            code: "",
-            data: {},
-            message: "",
-            error: {}
-        }
+export function createSuccessResponse(value: Record<string, string> | string) {
+    return {
+        status: "success",
+        code: "",
+        data: isString(value) ? {} : value,
+        message: isString(value) ? value : "",
+        errors: {}
+    }
+}
+
+export function createErrorResponse(value: Record<string, string>) {
+    return {
+        status: "error",
+        code: "",
+        data: {},
+        message: isString(value) ? value : "",
+        errors: value.errors || {}
     }
 }
